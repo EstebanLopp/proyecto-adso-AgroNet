d0785728802081aec3dafb72c87403fddbdfc964
Tatiana Gomez <yeimitatianagomezperez@gmail.com>
2025-08-16 16:30:09 -0500

    se agregaron los componentes

---
+++ frontend/public/js/components/producto_carrito.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const contenedor = document.getElementById('contenedor__componente');
+
+  if (contenedor) {
+    fetch('/frontend/public/views/producto_carrito.html')
+      .then(res => res.text())
+      .then(html => {
+        contenedor.innerHTML = html;
+      })
+      .catch(err => console.error('Error al cargar el producto:', err));
+  }
+});
---
+++ frontend/public/js/components/resumen.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const contenedor = document.querySelector('#resumen__producto__container');
+
+  if (contenedor) {
+    fetch('/frontend/public/views/resumen_producto.html')      
+    .then(response => response.text())
+      .then(data => {
+        contenedor.innerHTML = data;
+      })
+      .catch(error => console.error('Error cargando resumen-producto:', error));
+  }
+});
